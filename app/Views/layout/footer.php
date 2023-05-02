451c3253c42eb0f39f06b2034cd7e6f0aa5096cd
Doni Nurramdan <nurramdandoni@gmail.com>
2023-05-02 11:59:00 +0700

    templating and update info social

---
+++ app/Views/layout/footer.php
@@ -0,0 +1,100 @@
+<!-- ======= Footer ======= -->
+<footer id="footer" class="footer">
+
+<div class="footer-content">
+  <div class="container">
+    <div class="row">
+
+      <div class="col-lg-3 col-md-6">
+        <div class="footer-info">
+          <h3>Paylite</h3>
+          <p>
+            RT001 RW001 Dusun Jati Kidul <br>
+            Desa Jagara Kecamatan Darma, Kuningan 45562<br><br>
+            <strong>Phone:</strong> +62 89533 080 2566<br>
+            <strong>Email:</strong> info@paylite.id<br>
+          </p>
+        </div>
+      </div>
+
+      <div class="col-lg-2 col-md-6 footer-links">
+        <h4>Useful Links</h4>
+        <ul>
+          <li><i class="bi bi-chevron-right"></i> <a href="#">Home</a></li>
+          <li><i class="bi bi-chevron-right"></i> <a href="index.html#about">Tentang</a></li>
+          <li><i class="bi bi-chevron-right"></i> <a href="index.html#services">Layanan</a></li>
+          <li><i class="bi bi-chevron-right"></i> <a href="index.html#pricing">Harga Produk</a></li>
+          <li><i class="bi bi-chevron-right"></i> <a href="index.html#team">Tim Paylite</a></li>
+        </ul>
+      </div>
+
+      <div class="col-lg-3 col-md-6 footer-links">
+        <h4>Layanan</h4>
+        <ul>
+          <li><i class="bi bi-chevron-right"></i> <a href="index.html#services">Desain Website</a></li>
+          <li><i class="bi bi-chevron-right"></i> <a href="index.html#services">Pengembangan Web</a></li>
+          <li><i class="bi bi-chevron-right"></i> <a href="index.html#services">Database Adminitrator</a></li>
+          <li><i class="bi bi-chevron-right"></i> <a href="index.html#services">Integrasi Layanan</a></li>
+        </ul>
+      </div>
+
+      <div class="col-lg-4 col-md-6 footer-newsletter">
+        <h4>Langganan Informasi Produk</h4>
+        <p>Silahakan Subscribe untuk mendapatkan Informasi Produk dan Promosi Kami</p>
+        <form action="" method="post">
+          <input type="email" name="email"><input type="submit" value="Subscribe">
+        </form>
+
+      </div>
+
+    </div>
+  </div>
+</div>
+
+<div class="footer-legal text-center">
+  <div class="container d-flex flex-column flex-lg-row justify-content-center justify-content-lg-between align-items-center">
+
+    <div class="d-flex flex-column align-items-center align-items-lg-start">
+      <div class="copyright">
+        &copy; Copyright <strong><span>Paylite 2023</span></strong>. All Rights Reserved
+      </div>
+      <div class="credits">
+        <!-- All the links in the footer should remain intact. -->
+        <!-- You can delete the links only if you purchased the pro version. -->
+        <!-- Licensing information: https://bootstrapmade.com/license/ -->
+        <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/herobiz-bootstrap-business-template/ -->
+        Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>
+      </div>
+    </div>
+
+    <div class="social-links order-first order-lg-last mb-3 mb-lg-0">
+      <a target="_blank" href="https://twitter.com/Paylite220039" class="twitter"><i class="bi bi-twitter"></i></a>
+      <a target="_blank" href="https://web.facebook.com/profile.php?id=100092189448792" class="facebook"><i class="bi bi-facebook"></i></a>
+      <a targer="_blank" href="https://www.instagram.com/paylite2023/" class="instagram"><i class="bi bi-instagram"></i></a>
+      <!-- <a href="#" class="google-plus"><i class="bi bi-skype"></i></a> -->
+      <a target="_blank" href="https://www.linkedin.com/in/paylite-paylite-68609a275/" class="linkedin"><i class="bi bi-linkedin"></i></a>
+    </div>
+
+  </div>
+</div>
+
+</footer><!-- End Footer -->
+
+<a href="#" class="scroll-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+
+<div id="preloader"></div>
+
+<!-- Vendor JS Files -->
+<script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+<script src="assets/vendor/aos/aos.js"></script>
+<script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
+<script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+<script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
+<script src="assets/vendor/php-email-form/validate.js"></script>
+
+<!-- Template Main JS File -->
+<script src="assets/js/main.js"></script>
+
+</body>
+
+</html>
---
+++ app/Views/layout/menu.php
@@ -0,0 +1,63 @@
+<nav id="navbar" class="navbar">
+        <ul>
+
+          <!-- <li class="dropdown"><a href="#"><span>Home</span> <i class="bi bi-chevron-down dropdown-indicator"></i></a>
+            <ul>
+              <li><a href="index.html" class="active">Home 1 - index.html</a></li>
+              <li><a href="index-2.html">Home 2 - index-2.html</a></li>
+              <li><a href="index-3.html">Home 3 - index-3.html</a></li>
+              <li><a href="index-4.html">Home 4 - index-4.html</a></li>
+            </ul>
+          </li> -->
+
+          <li><a class="nav-link scrollto" href="#">Home</a></li>
+          <li><a class="nav-link scrollto" href="index.html#about">Tentang</a></li>
+          <li><a class="nav-link scrollto" href="index.html#services">Layanan</a></li>
+          <li><a class="nav-link scrollto" href="index.html#pricing">Harga Produk</a></li>
+          <li><a class="nav-link scrollto" href="index.html#team">Tim Paylite</a></li>
+          <!-- <li><a href="blog.html">Blog</a></li>
+          <li class="dropdown megamenu"><a href="#"><span>Mega Menu</span> <i class="bi bi-chevron-down dropdown-indicator"></i></a>
+            <ul>
+              <li>
+                <a href="#">Column 1 link 1</a>
+                <a href="#">Column 1 link 2</a>
+                <a href="#">Column 1 link 3</a>
+              </li>
+              <li>
+                <a href="#">Column 2 link 1</a>
+                <a href="#">Column 2 link 2</a>
+                <a href="#">Column 3 link 3</a>
+              </li>
+              <li>
+                <a href="#">Column 3 link 1</a>
+                <a href="#">Column 3 link 2</a>
+                <a href="#">Column 3 link 3</a>
+              </li>
+              <li>
+                <a href="#">Column 4 link 1</a>
+                <a href="#">Column 4 link 2</a>
+                <a href="#">Column 4 link 3</a>
+              </li>
+            </ul>
+          </li>
+          <li class="dropdown"><a href="#"><span>Drop Down</span> <i class="bi bi-chevron-down dropdown-indicator"></i></a>
+            <ul>
+              <li><a href="#">Drop Down 1</a></li>
+              <li class="dropdown"><a href="#"><span>Deep Drop Down</span> <i class="bi bi-chevron-down dropdown-indicator"></i></a>
+                <ul>
+                  <li><a href="#">Deep Drop Down 1</a></li>
+                  <li><a href="#">Deep Drop Down 2</a></li>
+                  <li><a href="#">Deep Drop Down 3</a></li>
+                  <li><a href="#">Deep Drop Down 4</a></li>
+                  <li><a href="#">Deep Drop Down 5</a></li>
+                </ul>
+              </li>
+              <li><a href="#">Drop Down 2</a></li>
+              <li><a href="#">Drop Down 3</a></li>
+              <li><a href="#">Drop Down 4</a></li>
+            </ul>
+          </li> -->
+          <li><a class="nav-link scrollto" href="index.html#contact">Kontak</a></li>
+        </ul>
+        <i class="bi bi-list mobile-nav-toggle d-none"></i>
+      </nav><!-- .navbar -->
